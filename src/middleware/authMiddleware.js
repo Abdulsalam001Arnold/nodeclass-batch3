1617d51cc42683f3d211f2ae1596e029461cc75d
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-11-03 12:40:45 +0100

    added

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,14 +4,19 @@ import { userModel } from '../models/userModel';
 
 const secret = process.env.JWT_SECRET
 
-export const protect = (req, res, next) =>  {
+export const protect = async(req, res, next) =>  {
     let token;
 
     try {
         if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, secret)
+            req.user = await userModel.findById(decoded.id).select('-password')
+            next()
         }
     } catch (err) {
-        
+        if(err instanceof Error) {
+            console.error(err.message)
+        }
     }
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,9 @@
 
 import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
 import { getHome, postForm, signUp, login } from '../controllers/userController.js';
 
 const userRouter = express.Router()
-userRouter.get('/', getHome).post('/submit-form', postForm).post('/signup', signUp).post("/login", login)
+userRouter.get('/', getHome).post('/submit-form', protect, postForm).post('/signup', signUp).post("/login", login)
 
 export default userRouter;
