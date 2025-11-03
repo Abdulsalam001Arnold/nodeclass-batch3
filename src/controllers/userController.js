bd4646890c418c9572644e10ef883b9bf8e830d1
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-11-03 13:22:52 +0100

    feat: added delete logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -123,3 +123,23 @@ export async function getSingle(req, res) {
         user
     })
 }
+
+export async function deleteUser(req, res) {
+    const { id } = req.params
+    if(!id) {
+        return res.status(400).json({
+            message: "User ID is required"
+        })
+    }
+    const deletedUser = await userModel.findByIdAndDelete(id)
+    if(!deletedUser) {
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "User deleted successfully",
+        deletedUser
+    })
+}
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,9 @@
 
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { getHome, postForm, signUp, login, getSingle } from '../controllers/userController.js';
+import { getHome, postForm, signUp, login, getSingle, deleteUser } from '../controllers/userController.js';
 
 const userRouter = express.Router()
-userRouter.get('/', getHome).post('/submit-form', protect, postForm).post('/signup', signUp).post("/login", login).get('/user/:id', getSingle)
+userRouter.get('/', getHome).post('/submit-form', protect, postForm).post('/signup', signUp).post("/login", login).get('/user/:id', getSingle).delete('/delete-user/:id', deleteUser)
 
 export default userRouter;
